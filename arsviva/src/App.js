0c2ca4cc03a4528a707af291a9e79086a7c95221
danicavladic <danica.vladic@fer.hr>
2020-09-23 16:50:29 +0200

    Dodani divovi.

--- arsviva/src/App.js
+++ arsviva/src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-mainDiv">
+      <div className="App-mainDiv-leftDiv"></div>
+      <div className="App-mainDiv-rightDiv" >
+        
+      </div>
+      </div>
     </div>
   );
 }
