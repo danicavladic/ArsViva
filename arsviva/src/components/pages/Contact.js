707d5d7e2a91b5407a3a6292ec7ffabb5d758820
Dorotea <dorotea.franjic@fer.hr>
2020-09-26 10:37:16 +0200

    Dodan google maps na Contact s adresom.

--- arsviva/src/components/pages/Contact.js
+++ arsviva/src/components/pages/Contact.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import '../../App.css';
 import Footer from '../Footer';
+import GoogleMapReact from 'google-map-react'
+import '../Map.css'
+import Map from '../Map';
 
+const location = {
+  address: 'Pete Poljanice 10, Zagreb, Croatia.',
+  lat: 45.828580,
+  lng: 16.072950,
+}
 
 function Contact() {
+  
   return (
     <>
-      
-   <Footer />
+    <div className="Mapa">
+    <Map location={location} zoomLevel={17} /> {/* include it here */}
+    <Footer />
+  </div>
+    
+   
       
     </>
   );
