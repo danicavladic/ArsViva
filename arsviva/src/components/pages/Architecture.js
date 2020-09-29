c5bbb71a4781af9a9376c9b3da02034733804765
Dorotea <dorotea.franjic@fer.hr>
2020-09-29 13:18:28 +0200

    boja.

--- arsviva/src/components/pages/Architecture.js
+++ arsviva/src/components/pages/Architecture.js
@@ -3,12 +3,17 @@ import '../../App.css';
 import Footer from '../Footer';
 import '../Architecture.css';
 import ProjectCards from '../ProjectCards'
+import project1 from '../project1.jpg'
 
 
 
 function Architecture() {
     return (
-        <div>
+       
+        <div className="neobojan">
+            <div className="crni-div">
+                <img className="main-pic" src={project1}></img>
+            </div>
             <h1 className="checkOut">CHECK OUT OUR PROJECTS</h1>
             <ProjectCards />
             <ProjectCards />
@@ -16,6 +21,7 @@ function Architecture() {
             <div className="help"></div>
             <hr></hr>
             <Footer />
+       
         </div>
     )
 }
