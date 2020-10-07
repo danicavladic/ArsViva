8802b68eb1493e115c787422151fa621fcf09dd0
Dorotea <dorotea.franjic@fer.hr>
2020-10-07 19:11:19 +0200

    bla

--- arsviva/src/components/pages/Architecture.js
+++ arsviva/src/components/pages/Architecture.js
@@ -4,6 +4,9 @@ import Footer from '../Footer';
 import '../Architecture.css';
 import ProjectCards from '../ProjectCards'
 import project1 from '../project1.jpg'
+import bathroom from '../7.jpg'
+import CardSimple from '../CardSimple'
+
 
 
 
@@ -14,24 +17,23 @@ function Architecture() {
     return (  
         <>     
         <div className="neobojan">
-            <p className="izdvojen">IZDVOJENI PROJEKT</p>
-            <p className="first">#1 <br></br>Obiteljska kuća</p>
+         
             <div className="crni-div">
+                <div className="bijeli-div">
+                <CardSimple src={project1}></CardSimple>
+                <CardSimple src={bathroom}></CardSimple>
+                <CardSimple></CardSimple>
+                <CardSimple></CardSimple>
+                </div>
             
             </div> 
             
-            <hr className="bijela-crta"></hr>
-            <hr className="bijela-crta-2"></hr>
-            <hr className="bijela-crta-3"></hr>
-            <hr className="bijela-crta-4"></hr>
-            <hr className="bijela-crta-5"></hr>
-            <hr className="bijela-crta-6"></hr>
             
             
 
         
             <h1 className="checkOut">CHECK OUT OUR PROJECTS</h1>
-            <ProjectCards />
+            
             <ProjectCards />
             <ProjectCards />
             <div className="help"></div>
