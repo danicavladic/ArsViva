eb665a803e057ef712020d24c7a2576e078e8c7f
danicavladic <danica.vladic@fer.hr>
2020-10-07 20:07:02 +0200

    COMMIT.

---
+++ arsviva/src/components/OtherProjects.js
@@ -0,0 +1,112 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+
+import Typography from "@material-ui/core/Typography";
+import pozadina from "./5.jpg"
+
+
+const useStyles = makeStyles((theme) => ({
+  
+  root: {
+    width: 800,
+    height: 330,
+    marginTop: "50px",
+    marginLeft: "40px",
+    marginBottom: "-3px",
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: "rgb(36,47,53)"
+    
+  },
+  media: {
+    height: 330,
+    width: 500
+    // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandOpen: {
+    transform: "rotate(180deg)"
+  },
+  tekst: {
+    marginRight: "-20px",
+   backgroundColor: "rgb(36,47,53)",
+   color: "white",
+    width:"200px",
+  },
+
+  
+ 
+}));
+
+export default function RecipeReviewCard(props) {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.media}
+        image={props.src}
+        title="Living room"
+      />
+      <CardContent >
+        
+      <Typography  className={classes.tekst} component="p">
+          WINTER FAIRYTALE
+        </Typography>
+        
+      </CardContent>
+      <Typography  className={classes.bla} component="p">
+          
+        </Typography>
+      <CardActions disableSpacing></CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
+            set aside for 10 minutes.
+          </Typography>
+          <Typography paragraph>
+            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
+            over medium-high heat. Add chicken, shrimp and chorizo, and cook,
+            stirring occasionally until lightly browned, 6 to 8 minutes.
+            Transfer shrimp to a large plate and set aside, leaving chicken and
+            chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
+            onion, salt and pepper, and cook, stirring often until thickened and
+            fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
+            cups chicken broth; bring to a boil.
+          </Typography>
+          <Typography paragraph>
+            Add rice and stir very gently to distribute. Top with artichokes and
+            peppers, and cook without stirring, until most of the liquid is
+            absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
+            shrimp and mussels, tucking them down into the rice, and cook again
+            without stirring, until mussels have opened and rice is just tender,
+            5 to 7 minutes more. (Discard any mussels that don’t open.)
+          </Typography>
+          <Typography>
+            Set aside off of the heat to let rest for 10 minutes, and then
+            serve.
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
--- arsviva/src/components/pages/Architecture.js
+++ arsviva/src/components/pages/Architecture.js
@@ -2,7 +2,6 @@ import React from 'react';
 import '../../App.css';
 import Footer from '../Footer';
 import '../Architecture.css';
-import ProjectCards from '../ProjectCards'
 import project1 from '../project1.jpg'
 import bathroom from '../7.jpg'
 import CardSimple from '../CardSimple'
@@ -21,9 +20,7 @@ function Architecture() {
             <div className="crni-div">
                 <div className="bijeli-div">
                 <CardSimple src={project1}></CardSimple>
-                <CardSimple src={bathroom}></CardSimple>
-                <CardSimple></CardSimple>
-                <CardSimple></CardSimple>
+                
                 </div>
             
             </div> 
@@ -33,9 +30,7 @@ function Architecture() {
 
         
             <h1 className="checkOut">CHECK OUT OUR PROJECTS</h1>
-            
-            <ProjectCards />
-            <ProjectCards />
+        
             <div className="help"></div>
             <hr></hr>
             <Footer />
