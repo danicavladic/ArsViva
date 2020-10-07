4630b799eda8dbcfe532ee2b2a8787b972d95d1b
danicavladic <danica.vladic@fer.hr>
2020-10-07 19:13:15 +0200

    fad

--- arsviva/src/components/pages/Painting.js
+++ arsviva/src/components/pages/Painting.js
@@ -1,91 +1,25 @@
 import React from 'react'
-import slika from '../slikaa.jpg'
-import '../Painting.css'
-import Footer from '../Footer';
+import '../Painting.css';
+import sl from '../3.jpg'
 
 function Painting() {
     return (
-        <>
-           <div>
-                <img className="jazavac" src={slika}></img>
-                <div className="jazaviDiv">
-                <hr className="jazavaCrta"></hr>
-                <hr className="drugaJazavaCrta"></hr>
-                <div className="divCrta"></div>
-                <p className="winter">WINTER</p>
-                <p className="fairytale">FAIRYTALE</p>
-                <p className="darkness">"Darkness bring out our emotions,</p>
-                <p className="mix">mix them up with night life potions,</p>
-                <p className="countless">so many times we catch the sunrise."</p>
-                <button className="projekt1"><div className="pomDiv1">SEE THIS PROJECT</div></button>
-                </div>
-
-                <div className="ikonice">
-                    <p className="title">Madness</p>
-                    <i class="fas fa-palette"></i>
-                    <div className="prva">So love that</div>
-                    <i class="fas fa-heart"></i>
-                    <div className="druga">Fire, memories </div>
-                    <div className="treca">Floating higher <i class="fas fa-home"></i></div>
-                    <div className="cetvrta">Good for me <i class="fas fa-id-badge"></i></div>
-                </div>
-
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-                <div>J</div>
-
-               
-                
-            </div>
-            <Footer/>
-        </>
-
-
-
+        <div>
+            <div className="zeleni"><img className="slicicaa" src={sl}></img></div>
+            
+           
+            
+        </div>
     )
 }
 
 export default Painting
 
 
-/* let popupRef = React.createRef()
+/*
 
-        const onShowPopUp = () => {
-            popupRef.show()
-        } */
+ <div className="zeleniDrugi"></div>
+            <div className="sivi"></div>
+            <img className="slicicaa" src={sl}></img>
 
-/*<StatusBar barStyle='dark-content'/>
-<SafeAreaView style={{flex: '1', alignItems: 'center', justifyContent: 'center', marginTop: '100px'}}>
-    <TouchableWithoutFeedback onPress={onShowPopUp}>
-        <Text style = {{backgroundColor: 'blue' ,color: 'black', fontSize: '20'}}>
-            Show PopUp
-         </Text>
-    </TouchableWithoutFeedback>
-    <PopUp ref = {(target) => popupRef = target}/>
-</SafeAreaView>*/
-
-/*   <div>
-                <img className="jazavac" src={slika}></img>
-                <div className="jazaviDiv"></div>
-            </div> */
+*/
